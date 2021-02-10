ff908f3538a6581ef069408ed13af0399e5a418e
Andrea <corro.andrea.1995@gmail.com>
2021-02-10 16:27:49 +0100

    API

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 var nuovo = new Vue({
   el:'#cont',
   data:{
+    ricerca: ''
+
 
   },
   methods:{
-    
+    trova(){
+      axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key:'7b637101d1d50dc2f48259a676913f3c',
+          // devo mettere il nome della queri
+          query: this.ricerca,
+          language: 'it-IT'
+        }
+      })
+      .then((result) =>{
+        console.log(result.data);
+      })
+      .catch((error) => alert('errori'))
+      axios
+      .get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key:'7b637101d1d50dc2f48259a676913f3c',
+          // devo mettere il nome della queri
+          query: this.ricerca,
+          language: 'it-IT'
+        }
+      })
+      .then((result) =>{
+        console.log(result.data);
+      })
+      .catch((error) => alert('errori'))
+    }
   }
 })
