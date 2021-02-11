100f09adaeaae31a61c0e0206e24753f2095bcc2
Andrea <corro.andrea.1995@gmail.com>
2021-02-11 12:58:12 +0100

    con id sotto forma di numeri

--- js/script.js
+++ js/script.js
@@ -24,9 +24,39 @@ var nuovo = new Vue({
       })
       console.log(this.types);
     })
-    // axios 
-    // .get('')
-    //
+    axios
+    .get('https://api.themoviedb.org/3/search/movie?api_key=7b637101d1d50dc2f48259a676913f3c&language=en-US&query=matrix')
+    .then((result) => {
+        this.arrayFilm = result.data.results
+        this.arrayFilm.forEach((item, i) => {
+          item.stelle = [];
+          // immagine
+          // item.poster_path = 'http://image.tmdb.org/t/p/w500/'+ item.poster_path;
+          if (item.poster_path == null){
+            item.poster_path = 'https://media.istockphoto.com/vectors/coming-soon-vector-template-design-vector-id984996502?k=6&m=984996502&s=612x612&w=0&h=wbMAxPZZRht9DPspDS1NvM4TLlBCCajXnJfSmcckqZ8='
+          } else{
+            item.poster_path = 'http://image.tmdb.org/t/p/w500/'+ item.poster_path;
+          }
+          // bandiera
+          if (item.original_language == 'es'){
+            item.original_language = 'https://www.resolfin.com/wp-content/uploads/2018/02/spagna-100x150.png'
+          } else if (item.original_language == 'fr'){
+            item.original_language = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/280px-Flag_of_France.svg.png'
+          } else{
+            item.original_language = 'https://theitaliancommunity.co.uk/blog/wp-content/uploads/bandiera-inghilterra.png'
+          }
+          // stelle
+          item.vote_average = Math.ceil(item.vote_average / 2);
+          if (item.vote_average < 1){
+            item.vote_average = 1
+          }
+          for (var i = 0; i < item.vote_average; i++){
+            item.stelle.push('stella')
+          }
+
+        });
+    })
+
   },
   methods:{
     trova(){
@@ -128,7 +158,9 @@ var nuovo = new Vue({
     seleziona(){
       this.type = document.getElementById('opzione').value;
       console.log(this.type);
-      this.type = parseInt(this.type)
+      if (this.type != 'all') {
+        this.type = parseInt(this.type)
+      }
       console.log(this.type);
     }
 
