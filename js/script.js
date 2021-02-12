7b61d0a4d818945632c3b4a8bae0c21cfd496e41
Andrea <corro.andrea.1995@gmail.com>
2021-02-12 10:30:17 +0100

    unito array serie e film, aggiunta corrispondenza nome-id per categoria

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var nuovo = new Vue({
   data:{
     ricerca: '',
     arrayFilm: '',
-    arraySerie: '',
+    arrayTutto: '',
     immagine: 'none',
     descr: 'none',
     indice: '',
@@ -16,19 +16,37 @@ var nuovo = new Vue({
     axios
     .get('https://api.themoviedb.org/3/genre/movie/list?api_key=7b637101d1d50dc2f48259a676913f3c&language=en-US')
     .then((result)=> {
-      console.log(result.data.genres);
+      // console.log(result.data.genres);
       result.data.genres.forEach((item, i) => {
-        if(this.types.includes(item.id) == false){
-          this.types.push(item.id)
+
+        if(this.types.includes(item.name) == false){
+          this.types.push(item.name)
         }
       })
-      console.log(this.types);
+      // console.log(this.types);
     })
     axios
     .get('https://api.themoviedb.org/3/search/movie?api_key=7b637101d1d50dc2f48259a676913f3c&language=en-US&query=matrix')
     .then((result) => {
-        this.arrayFilm = result.data.results
-        this.arrayFilm.forEach((item, i) => {
+        this.arrayTutto = result.data.results
+        console.log(this.arrayTutto);
+        this.arrayTutto.forEach((item, i) => {
+          var elemento = item;
+
+          axios
+          .get('https://api.themoviedb.org/3/genre/movie/list?api_key=7b637101d1d50dc2f48259a676913f3c&language=en-US')
+          .then((result)=> {
+            // console.log(result.data.genres);
+            result.data.genres.forEach((item, i) => {
+
+              if(elemento.genre_ids.includes(item.id) == true){
+                elemento.genre_ids.push(item.name)
+              }
+            })
+            console.log(elemento.genre_ids);
+            // console.log(this.types);
+          })
+          // console.log(item.genre_ids);
           item.stelle = [];
           // immagine
           // item.poster_path = 'http://image.tmdb.org/t/p/w500/'+ item.poster_path;
@@ -72,33 +90,33 @@ var nuovo = new Vue({
       .then((result) =>{
         this.arrayFilm = result.data.results
         console.log(this.arrayFilm);
-        this.arrayFilm.forEach((item, i) => {
-          item.stelle = [];
-          // immagine
-          // item.poster_path = 'http://image.tmdb.org/t/p/w500/'+ item.poster_path;
-          if (item.poster_path == null){
-            item.poster_path = 'https://media.istockphoto.com/vectors/coming-soon-vector-template-design-vector-id984996502?k=6&m=984996502&s=612x612&w=0&h=wbMAxPZZRht9DPspDS1NvM4TLlBCCajXnJfSmcckqZ8='
-          } else{
-            item.poster_path = 'http://image.tmdb.org/t/p/w500/'+ item.poster_path;
-          }
-          // bandiera
-          if (item.original_language == 'es'){
-            item.original_language = 'https://www.resolfin.com/wp-content/uploads/2018/02/spagna-100x150.png'
-          } else if (item.original_language == 'fr'){
-            item.original_language = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/280px-Flag_of_France.svg.png'
-          } else{
-            item.original_language = 'https://theitaliancommunity.co.uk/blog/wp-content/uploads/bandiera-inghilterra.png'
-          }
-          // stelle
-          item.vote_average = Math.ceil(item.vote_average / 2);
-          if (item.vote_average < 1){
-            item.vote_average = 1
-          }
-          for (var i = 0; i < item.vote_average; i++){
-            item.stelle.push('stella')
-          }
-
-        });
+        // this.arrayFilm.forEach((item, i) => {
+        //   item.stelle = [];
+        //   // immagine
+        //   // item.poster_path = 'http://image.tmdb.org/t/p/w500/'+ item.poster_path;
+        //   if (item.poster_path == null){
+        //     item.poster_path = 'https://media.istockphoto.com/vectors/coming-soon-vector-template-design-vector-id984996502?k=6&m=984996502&s=612x612&w=0&h=wbMAxPZZRht9DPspDS1NvM4TLlBCCajXnJfSmcckqZ8='
+        //   } else{
+        //     item.poster_path = 'http://image.tmdb.org/t/p/w500/'+ item.poster_path;
+        //   }
+        //   // bandiera
+        //   if (item.original_language == 'es'){
+        //     item.original_language = 'https://www.resolfin.com/wp-content/uploads/2018/02/spagna-100x150.png'
+        //   } else if (item.original_language == 'fr'){
+        //     item.original_language = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/280px-Flag_of_France.svg.png'
+        //   } else{
+        //     item.original_language = 'https://theitaliancommunity.co.uk/blog/wp-content/uploads/bandiera-inghilterra.png'
+        //   }
+        //   // stelle
+        //   item.vote_average = Math.ceil(item.vote_average / 2);
+        //   if (item.vote_average < 1){
+        //     item.vote_average = 1
+        //   }
+        //   for (var i = 0; i < item.vote_average; i++){
+        //     item.stelle.push('stella')
+        //   }
+        //
+        // });
       })
       .catch((error) => alert('errori'))
       axios
@@ -111,9 +129,24 @@ var nuovo = new Vue({
         }
       })
       .then((result) =>{
-        this.arraySerie = result.data.results
-        console.log(result.data.results);
-        this.arraySerie.forEach((item, i) => {
+        this.arrayTutto = [...this.arrayFilm,...result.data.results]
+        console.log(this.arrayTutto);
+        this.arrayTutto.forEach((item, i) => {
+          var elemento = item;
+
+          axios
+          .get('https://api.themoviedb.org/3/genre/movie/list?api_key=7b637101d1d50dc2f48259a676913f3c&language=en-US')
+          .then((result)=> {
+            // console.log(result.data.genres);
+            result.data.genres.forEach((item, i) => {
+
+              if(elemento.genre_ids.includes(item.id) == true){
+                elemento.genre_ids.push(item.name)
+              }
+            })
+            console.log(elemento.genre_ids);
+            // console.log(this.types);
+          })
           item.stelle = [];
           // immagini
           // console.log(item);
@@ -128,8 +161,10 @@ var nuovo = new Vue({
             item.original_language = 'https://www.resolfin.com/wp-content/uploads/2018/02/spagna-100x150.png'
           } else if (item.original_language == 'fr'){
             item.original_language = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/280px-Flag_of_France.svg.png'
-          } else{
+          } else if (item.original_language == 'en'){
             item.original_language = 'https://theitaliancommunity.co.uk/blog/wp-content/uploads/bandiera-inghilterra.png'
+          } else {
+            item.original_language ='https://images-na.ssl-images-amazon.com/images/I/61z5tXoJWXL._AC_SY450_.jpg'
           }
           // stelle
           item.vote_average = Math.ceil(item.vote_average / 2);
@@ -158,9 +193,9 @@ var nuovo = new Vue({
     seleziona(){
       this.type = document.getElementById('opzione').value;
       console.log(this.type);
-      if (this.type != 'all') {
-        this.type = parseInt(this.type)
-      }
+      // if (this.type != 'all') {
+      //   this.type = parseInt(this.type)
+      // }
       console.log(this.type);
     }
 
